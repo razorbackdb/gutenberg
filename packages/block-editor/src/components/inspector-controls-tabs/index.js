55d69166b32e86a370dd3dfbaa977cf2b1d6f6bf
Daniel Richards <daniel.richards@automattic.com>
2025-09-30 15:56:06 +0800

    Fix showing the editable fields in the content tab

--- packages/block-editor/src/components/inspector-controls-tabs/index.js
+++ packages/block-editor/src/components/inspector-controls-tabs/index.js
@@ -108,7 +108,10 @@ export default function InspectorControlsTabs( {
 					/>
 				</Tabs.TabPanel>
 				<Tabs.TabPanel tabId={ TAB_CONTENT.name } focusable={ false }>
-					<ContentTab contentClientIds={ contentClientIds } />
+					<ContentTab
+						rootClientId={ clientId }
+						contentClientIds={ contentClientIds }
+					/>
 				</Tabs.TabPanel>
 				<Tabs.TabPanel tabId={ TAB_LIST_VIEW.name } focusable={ false }>
 					<InspectorControls.Slot group="list" />
